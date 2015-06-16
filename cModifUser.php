d64c3ccaf0f573448ad8eeb28bcf60fd15300507
marvny <marvyn.francesconi@outlook.fr>
2015-06-16 11:49:22 +0200

    tr

--- cModifUser.php
+++ cModifUser.php
@@ -58,6 +58,7 @@
                     <?php
                     $sql = "SELECT adresse, cp, ville FROM visiteur WHERE id = '".$select."'";
                     echo $sql;
+                    echo $select;
                     // on lance la requête (mysql_query) et on impose un message d'erreur si la requête ne se passe pas bien (or die)
                     $req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
 
